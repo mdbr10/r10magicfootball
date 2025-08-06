3eacef089ecc8abc43632fc12ba6c7b41c1cfdd3
rscarlosjunior <rs.carlosjunior@gmail.com>
2025-08-06 18:33:43 -0300

    corrigindo dominio

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/links',
-        has: [
-          {
-            type: 'host',
-            value: '^(www\\.)?r10magicfootball\\.com\\$',
-          },
-        ],
-        destination: 'https://www.r10magicfootball.com',
-        permanent: false, // true para 308 (permanente), false para 307 (temporário)
-      }
-    ];
-  }
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
